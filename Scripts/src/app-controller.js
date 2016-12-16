66bf079e837b87b26b5a9286f5b3849cbd9429be
erann1987 <erann1987@gmail.com>
2016-12-16 02:09:01 +0200

    fixed report creation

--- Scripts/src/app-controller.js
+++ Scripts/src/app-controller.js
@@ -3,88 +3,103 @@
 
 	    $scope.chart;
 
-
-	    $scope.dtOptions = DTOptionsBuilder.newOptions()
-        .withDisplayLength(10)
-        .withLanguage({
-            "processing": "מעבד...",
-            "lengthMenu": "הצג _MENU_ פריטים",
-            "zeroRecords": "לא נמצאו רשומות מתאימות",
-            "emptyTable": "לא נמצאו רשומות מתאימות",
-            "info": "_START_ עד _END_ מתוך _TOTAL_ רשומות",
-            "infoEmpty": "0 עד 0 מתוך 0 רשומות",
-            "infoFiltered": "(מסונן מסך _MAX_  רשומות)",
-            "infoPostFix": "",
-            "search": "חפש:",
-            "url": "",
-            "paginate": {
-                "first": "ראשון",
-                "previous": "קודם",
-                "next": "הבא",
-                "last": "אחרון"
-            }
-        })
-        .withOption('bLengthChange', false);
-
-
-
-	    $scope.areaChart = {
+	    $scope.lineChart = {
 	        categories: [],
 	        series: []
-	    }
-
-	    $scope.renderDataAreaChart = function () {
-	        var size = $scope.report.cEndYear - $scope.report.cStartYear + 1;
-	        for (i = 0; i < size; i++) {
-	            $scope.areaChart.categories.push(parseInt($scope.report.cStartYear) + parseInt(i));
-	        }
-	        for (j = 0; j < $scope.report.data.length ; j = j + size) {
-	            $scope.areaChart.series.push({
-	                name: $scope.report.data[j].name,
-	                data: []
-	            });
-	        }
-	        for (i = 0; i < $scope.areaChart.series.length; i++)
-	            for (j = 0; j < size; j++)
-	                $scope.areaChart.series[i].data.push($scope.report.data[i + j].y);
 	    }
 
 	    $scope.report = {
 	        reportType: [],
 	        categories: [],
+            categoryDesc:[],
 	        years: ['2012', '2013', '2014', '2015', '2016'],
 	        cCategory: null,
+            cCategoryDesc: '',
 	        cReportType: [],
 	        cStartYear: null,
 	        cEndYear: null,
 	        data: []
 	    }
-
-	    $scope.alerts = [
-            { type: 'danger', msg: 'testing alerts!' },
-            { type: 'success', msg: 'testing alerts!' }
-	    ];
-	    $scope.addAlert = function (ty, ms) {
-	        $scope.alerts.push({ type: ty, msg: ms });
-	    };
-	    $scope.closeAlert = function (index) {
-	        $scope.alerts.splice(index, 1);
-	    };
-
-
-	    $scope.dataList = [];
 	    $scope.showReport = false;
-	    $scope.showDataTable = false;
+	    catDescChoosed = false;
 
-	    $scope.createReport = function () {
+
+        //get list of categories descriptions
+	    $scope.getCatDesc = function()
+	    {
 	        $.loader({
 	            className: "blue-with-image-2",
 	            content: ''
 	        });
-	        $scope.getAllCategories();
-	        $scope.getAllValueType();
+	        AppService.GetCategoriesDes($scope.report.cCategory.CategoryName).then(function (results) {
+	            $scope.report.categoryDesc = results.data;
+	            $.loader('close');
+	        }, function (e) {
+	            $.loader('close');
+	            alert("getting categories failed");
+	        });
 	    }
 
+	    $scope.dataList = [];
+	    $scope.showDataTable = false;
+
+	    //get data for create report modal
+	    $scope.createReportButton = function () {
+	        $.loader({
+	            className: "blue-with-image-2",
+	            content: ''
+	        });
+	        AppService.GetCreateReportModalData().then(function (results) {
+	            $scope.report.reportType = results.data.valueTypes;
+	            $scope.report.categories = results.data.categories;
+	            $scope.report.years = results.data.years;
+	            $scope.$watch(function () {
+	                $('.selectpicker').selectpicker('refresh');
+	            });
+	            $.loader('close');
+	        }, function (e) {
+	            $.loader('close');
+	            alert("getting categories failed");
+	        });
+	    }
+
+        //load relevant data for report
+	    $scope.loadRelevantData = function () {
+	        $.loader({
+	            className: "blue-with-image-2",
+	            content: ''
+	        });
+	        AppService.GetRelevantData($scope.report).then(function (results) {
+	            $scope.report.data = results.data;
+	            $scope.renderDataForChart();
+	            $scope.loadLineChart();
+	            $scope.showReport = true;
+	            $.loader('close');
+	        }, function (e) {
+	            $.loader('close');
+	            alert("getting categories failed");
+	        });
+	    }
+
+	    //render data for chart
+	    $scope.renderDataForChart = function () {
+	        $scope.lineChart.categories = ['ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני', 'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר'];
+	        var seriesSize = $scope.report.cEndYear - $scope.report.cStartYear + 1;
+	        for (i = $scope.report.cStartYear; i <= $scope.report.cEndYear; i++) {
+	            var valArray = Enumerable.From($scope.report.data)
+                    .Where(function (x) { return x.Year == i })
+                    .OrderBy(function (x) { return x.Month })
+                    .Select(function (x) { return x.Value })
+                    .ToArray();
+	            var seria = {
+	                name: i,
+	                data: valArray
+	            }
+	            $scope.lineChart.series.push(seria);
+	        }
+	    }
+
+	    //get all the data form db
 	    $scope.getAllData = function () {
 	        $.loader({
 	            className: "blue-with-image-2",
@@ -99,43 +114,56 @@
 	        });
 	    }
 
-	    $scope.getAllCategories = function () {
-	        AppService.GetAllCategories().then(function (results) {
-	            $scope.report.categories = results.data;
-	            $.loader('close');
-	        }, function (e) {
-	            $.loader('close');
-	            alert("getting categories failed");
-	        });
-	    }
+        //charts:
 
-	    $scope.getAllValueType = function () {
-	        AppService.GetAllValueType().then(function (results) {
-	            $scope.report.reportType = results.data;
-	            $scope.$watch(function () {
-	                $('.selectpicker').selectpicker('refresh');
-	            });
-	            $.loader('close');
-	        }, function (e) {
-	            $.loader('close');
-	            alert("getting categories failed");
-	        });
-	    }
-
-	    $scope.loadRelevantData = function () {
-	        $.loader({
-	            className: "blue-with-image-2",
-	            content: ''
-	        });
-	        AppService.GetRelevantData($scope.report).then(function (results) {
-	            $scope.report.data = results.data;
-	            $scope.renderDataAreaChart();
-	            $scope.loadLineChart();
-	            $scope.showReport = true;
-	            $.loader('close');
-	        }, function (e) {
-	            $.loader('close');
-	            alert("getting categories failed");
+	    $scope.loadLineChart = function () {
+	         $scope.chart = Highcharts.chart('lineChart', {
+	            credits: {
+	                text: 'ערן התותח',
+	                href: 'https://www.facebook.com/Eran.Math.teacher/'
+	            },
+	            title: {
+	                text: 'גרף ' + $scope.report.cReportType + ' שנתי ',
+	                x: -20, //center
+	                useHTML: Highcharts.hasBidiBug
+	            },
+	            subtitle: {
+	                text: $scope.report.cCategory.CategoryName + ': ' + $scope.report.cCategoryDesc,
+	                x: -20
+	            },
+	            xAxis: {
+	                categories: $scope.lineChart.categories,
+	                reversed: true,
+	                title: {
+	                    text: 'חודש'
+	                }
+	            },
+	            yAxis: {
+	                title: {
+	                    text: '(ש"ח)',
+	                    useHTML: Highcharts.hasBidiBug
+	                },
+	                plotLines: [{
+	                    value: 0,
+	                    width: 1,
+	                    color: '#808080'
+	                }],
+	                opposite: true,
+	            },
+	            legend: {
+	                layout: 'vertical',
+	                align: 'left',
+	                verticalAlign: 'middle',
+	                borderWidth: 0,
+	                rtl: true,
+	                useHTML: Highcharts.hasBidiBug
+	            },
+	            tooltip: {
+	                useHTML: true,
+	                headerFormat: '<small>{point.key}</small><table>',
+	                pointFormat: '<tr><td style="color: {series.color}"> {point.y} שח &nbsp;</td>' + '<td style="text-align: right"><b>  :{series.name}</b></td></tr>'
+	            },
+	            series: $scope.lineChart.series
 	        });
 	    }
 
@@ -164,57 +192,6 @@
 	                name: 'הוצאות',
 	                data: [513434, 646337, 344653]
 	            }]
-	        });
-	    }
-
-	    $scope.loadLineChart = function () {
-	         $scope.chart = Highcharts.chart('lineChart', {
-	            credits: {
-	                text: 'ערן התותח',
-	                href: 'https://www.facebook.com/Eran.Math.teacher/'
-	            },
-	            title: {
-	                text: 'גרף ' + $scope.report.cReportType + ' שנתי ',
-	                x: -20, //center
-	                useHTML: Highcharts.hasBidiBug
-	            },
-	            subtitle: {
-	                text: 'לפי ' + $scope.report.cCategory.CategoryName,
-	                x: -20
-	            },
-	            xAxis: {
-	                categories: $scope.areaChart.categories,
-	                reversed: true,
-	                title: {
-	                    text: 'שנים'
-	                }
-	            },
-	            yAxis: {
-	                title: {
-	                    text: '(ש"ח)',
-	                    useHTML: Highcharts.hasBidiBug
-	                },
-	                plotLines: [{
-	                    value: 0,
-	                    width: 1,
-	                    color: '#808080'
-	                }],
-	                opposite: true,
-	            },
-	            legend: {
-	                layout: 'vertical',
-	                align: 'left',
-	                verticalAlign: 'middle',
-	                borderWidth: 0,
-	                rtl: true,
-	                useHTML: Highcharts.hasBidiBug
-	            },
-	            tooltip: {
-	                useHTML: true,
-	                headerFormat: '<small>{point.key}</small><table>',
-	                pointFormat: '<tr><td style="color: {series.color}"> {point.y} שח &nbsp;</td>' + '<td style="text-align: right"><b>  :{series.name}</b></td></tr>'
-	            },
-	            series: $scope.areaChart.series
 	        });
 	    }
 
@@ -264,5 +241,41 @@
 	            }]
 	        });
 	    }
+
+	    //alerts
+	    $scope.alerts = [
+            { type: 'danger', msg: 'testing alerts!' },
+            { type: 'success', msg: 'testing alerts!' }
+	    ];
+	    $scope.addAlert = function (ty, ms) {
+	        $scope.alerts.push({ type: ty, msg: ms });
+	    };
+	    $scope.closeAlert = function (index) {
+	        $scope.alerts.splice(index, 1);
+	    };
+
+
+	    //data-tables configuration
+	    $scope.dtOptions = DTOptionsBuilder.newOptions()
+        .withDisplayLength(10)
+        .withLanguage({
+            "processing": "מעבד...",
+            "lengthMenu": "הצג _MENU_ פריטים",
+            "zeroRecords": "לא נמצאו רשומות מתאימות",
+            "emptyTable": "לא נמצאו רשומות מתאימות",
+            "info": "_START_ עד _END_ מתוך _TOTAL_ רשומות",
+            "infoEmpty": "0 עד 0 מתוך 0 רשומות",
+            "infoFiltered": "(מסונן מסך _MAX_  רשומות)",
+            "infoPostFix": "",
+            "search": "חפש:",
+            "url": "",
+            "paginate": {
+                "first": "ראשון",
+                "previous": "קודם",
+                "next": "הבא",
+                "last": "אחרון"
+            }
+        })
+        .withOption('bLengthChange', false);
 
 	}]);
